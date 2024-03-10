112fdca1de2bbc7e692f3355abeb1eeb1f901df6
StarTux <ths.loy@gmail.com>
2024-03-10 23:39:08 +0100

    Bamboo sign

--- src/main/java/com/cavetale/egg/Game.java
+++ src/main/java/com/cavetale/egg/Game.java
@@ -97,15 +97,16 @@ public final class Game {
         .collect(Collectors.toList());
     protected List<Material> signs = Arrays
         .asList(Material.ACACIA_SIGN,
+                Material.BAMBOO_SIGN,
                 Material.BIRCH_SIGN,
+                Material.CHERRY_SIGN,
+                Material.CRIMSON_SIGN,
                 Material.DARK_OAK_SIGN,
                 Material.JUNGLE_SIGN,
+                Material.MANGROVE_SIGN,
                 Material.OAK_SIGN,
                 Material.SPRUCE_SIGN,
-                Material.CRIMSON_SIGN,
-                Material.WARPED_SIGN,
-                Material.MANGROVE_SIGN,
-                Material.CHERRY_SIGN);
+                Material.WARPED_SIGN);
     protected List<Material> dirts = List.of(new Material[] {
             Material.DIRT,
             Material.COARSE_DIRT,
